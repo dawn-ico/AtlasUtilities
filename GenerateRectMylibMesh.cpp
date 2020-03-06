32678d8cf36ffa33822f98c1b5f588c09009c358
Matthias Roethlin <Matthias.Roethlin@MeteoSwiss.ch>
2020-03-06 08:40:57 +0100

    cleaned up mylib rect mesh

--- GenerateRectMylibMesh.cpp
+++ GenerateRectMylibMesh.cpp
@@ -1,1 +1,72 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
 #include "GenerateRectMylibMesh.h"
+
+mylib::Grid MylibMeshRect(int ny) {
+  bool periodic = false;
+  const auto& mesh = mylib::Grid(3 * ny, ny, periodic);
+
+  double newHeight = (ny - 1) * sqrt(3) / 2.;
+  double length = newHeight * 2;
+
+  std::tuple<double, double> bblo{0., -std::numeric_limits<double>::max()};
+  std::tuple<double, double> bbhi{length + 0.1 * (length / (2 * ny)),
+                                  std::numeric_limits<double>::max()};
+
+  auto inBB = [&](double x, double y) {
+    return x > std::get<0>(bblo) && y > std::get<1>(bblo) && x < std::get<0>(bbhi) &&
+           y < std::get<1>(bbhi);
+  };
+  auto anyInBB = [&](const mylib::Face& f) {
+    for(const auto& v : f.vertices()) {
+      if(inBB(v->x(), v->y())) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  std::vector<mylib::Face> keptCells;
+  std::vector<int> keptIndices;
+  std::copy_if(mesh.faces().begin(), mesh.faces().end(), std::back_inserter(keptCells), anyInBB);
+  std::transform(keptCells.begin(), keptCells.end(), std::back_inserter(keptIndices),
+                 [](const mylib::Face& c) { return c.id(); });
+  auto rectMesh = mylib::Grid(mesh, keptIndices);
+  double xMin =
+      std::min_element(rectMesh.vertices().begin(), rectMesh.vertices().end(),
+                       [](const mylib::Vertex& a, const mylib::Vertex& b) { return a.x() < b.x(); })
+          ->x();
+  double xMax =
+      std::max_element(rectMesh.vertices().begin(), rectMesh.vertices().end(),
+                       [](const mylib::Vertex& a, const mylib::Vertex& b) { return a.x() < b.x(); })
+          ->x();
+  double yMin =
+      std::min_element(rectMesh.vertices().begin(), rectMesh.vertices().end(),
+                       [](const mylib::Vertex& a, const mylib::Vertex& b) { return a.y() < b.y(); })
+          ->y();
+  double yMax =
+      std::max_element(rectMesh.vertices().begin(), rectMesh.vertices().end(),
+                       [](const mylib::Vertex& a, const mylib::Vertex& b) { return a.y() < b.y(); })
+          ->y();
+
+  double lX = xMax - xMin;
+  double lY = yMax - yMin;
+  // re-center
+  rectMesh.shift(-xMin - lX / 2, -yMin - lY / 2);
+  // scale (single scale factor to exactly preserve equilateral edge lengths)
+  double scale = 180 / lY;
+  rectMesh.scale(scale);
+  return rectMesh;
+}
--- GenerateRectMylibMesh.h
+++ GenerateRectMylibMesh.h
@@ -12,86 +12,9 @@
 //
 //===------------------------------------------------------------------------------------------===//
 
-#include <assert.h>
-#include <iostream>
-#include <optional>
-#include <string>
-
 #include "mylib.hpp"
 
-namespace {
-void debugDump(const mylib::Grid& mesh, const std::string prefix) {
-  {
-    char buf[256];
-    sprintf(buf, "%sT.txt", prefix.c_str());
-    FILE* fp = fopen(buf, "w+");
-    for(const auto& cellIt : mesh.faces()) {
-      fprintf(fp, "%d %d %d\n", cellIt.vertex(0).id() + 1, cellIt.vertex(1).id() + 1,
-              cellIt.vertex(2).id() + 1);
-    }
-    fclose(fp);
-  }
+// Generate a equilateral structured triangle mesh using our toy library with nx = 2*ny
+// and [xmin, xmax] = [-180,180], [ymin, max] = [-90, 90]
 
-  {
-    char buf[256];
-    sprintf(buf, "%sP.txt", prefix.c_str());
-    FILE* fp = fopen(buf, "w+");
-    for(const auto& nodeIt : mesh.vertices()) {
-      double x = nodeIt.x();
-      double y = nodeIt.y();
-      fprintf(fp, "%f %f \n", x, y);
-    }
-    fclose(fp);
-  }
-}
-} // namespace
-
-mylib::Grid MylibMeshRect(int ny) {
-  const auto& mesh = mylib::Grid(3 * ny, ny, false);
-
-  double newHeight = (ny - 1) * sqrt(3) / 2.;
-  double length = newHeight * 2;
-
-  std::vector<int> keep;
-  std::tuple<double, double> bblo{0., -std::numeric_limits<double>::max()};
-  std::tuple<double, double> bbhi{length + 0.1 * (length / (2 * ny)),
-                                  std::numeric_limits<double>::max()};
-  auto inBB = [&](double x, double y) {
-    return x > std::get<0>(bblo) && y > std::get<1>(bblo) && x < std::get<0>(bbhi) &&
-           y < std::get<1>(bbhi);
-  };
-  for(const auto cellIt : mesh.faces()) {
-    bool in = false;
-    for(const auto nIt : cellIt.vertices()) {
-      if(inBB(nIt->x(), nIt->y())) {
-        in = true;
-        break;
-      }
-    }
-    if(in) {
-      keep.push_back(cellIt.id());
-    }
-  }
-
-  auto rectMesh = mylib::Grid(mesh, keep);
-  double xMin = std::numeric_limits<double>::max();
-  double yMin = std::numeric_limits<double>::max();
-  double xMax = -std::numeric_limits<double>::max();
-  double yMax = -std::numeric_limits<double>::max();
-  for(auto nodeIt : rectMesh.vertices()) {
-    double x = nodeIt.x();
-    double y = nodeIt.y();
-    xMin = fmin(x, xMin);
-    yMin = fmin(y, yMin);
-    xMax = fmax(x, xMax);
-    yMax = fmax(y, yMax);
-  }
-  double lX = xMax - xMin;
-  double lY = yMax - yMin;
-  // re-center
-  rectMesh.shift(-xMin - lX / 2, -yMin - lY / 2);
-  // scale (single scale factor to exactly preserve equilateral edge lengths)
-  double scale = 180 / lY;
-  rectMesh.scale(scale);
-  return rectMesh;
-}
+mylib::Grid MylibMeshRect(int ny);
